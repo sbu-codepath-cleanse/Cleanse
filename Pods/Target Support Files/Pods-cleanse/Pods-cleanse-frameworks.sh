05dfdbfd69fd4bdfcf93e1cef17e2674e5547e27
vgoel30 <varun.goel@stonybrook.edu>
2016-04-19 19:26:31 -0400

    added twitter stuff merge in process

--- Pods/Target Support Files/Pods-cleanse/Pods-cleanse-frameworks.sh
+++ Pods/Target Support Files/Pods-cleanse/Pods-cleanse-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-cleanse/Bolts.framework"
+  install_framework "Pods-cleanse/MDCSwipeToChoose.framework"
   install_framework "Pods-cleanse/Parse.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-cleanse/Bolts.framework"
+  install_framework "Pods-cleanse/MDCSwipeToChoose.framework"
   install_framework "Pods-cleanse/Parse.framework"
 fi
